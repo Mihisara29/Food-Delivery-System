b0d2f7282ee5b2daa6b40ee655ff364a6de56675
Mihisara29 <146804234+Mihisara29@users.noreply.github.com>
2025-03-28 13:37:16 +0530

    Created components for side bar,menu bar,And also created components for add food,list food and orders.Until now I completed Add food part for the admin.As well as I took the help of packages like bootstrap,react-toastify.Now Admin can add food to database across the backend api.

---
+++ adminpanel/src/services/foodService.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const API_URL = 'http://localhost:8080/api/foods';
+
+export const addFood = async(foodData,image) =>{
+
+  const formData = new FormData();
+  formData.append('food',JSON.stringify(foodData))
+  formData.append('file',image);
+
+ try{
+       const response = await axios.post(
+         API_URL,
+         formData,
+         {
+           headers: {
+             "Content-Type": "multipart/form-data"
+           }
+         }
+       );
+ 
+       
+     }catch(error){
+ 
+      console.log('Error',error); 
+      throw error;
+ 
+ 
+     }
+
+}
