0f31a27db149dfcc3005356a9cf7d1d870a05e5a
Mihisara29 <146804234+Mihisara29@users.noreply.github.com>
2025-04-30 15:26:40 +0530

    completed ExploreFood page. User can search the food which they like and in home page user can see foods under different catagories.As well as created object for the cart and create tow function for dcrease and increase cart items in StoreContext.jsx

--- foodies/src/context/StoreContext.jsx
+++ foodies/src/context/StoreContext.jsx
@@ -7,7 +7,19 @@ export const StoreContext = createContext(null);
 export const StoreContextProvider = (props) => {
     
   const [foodList,setFoodList] = useState([]);
+  const [quantities,setQuantities] = useState({});
 
+  const increaseQuantity = (foodId) => {
+      setQuantities((prev) => ({...prev,[foodId]:(prev[foodId] || 0)+1}))
+  }
+
+  const decreaseQuantity = (foodId) => {
+    setQuantities((prev) => ({
+      ...prev,
+      [foodId]: prev[foodId] > 0 ? prev[foodId] - 1 : 0
+    }));
+  };
+  
   const contextValue = {
       foodList
   };
